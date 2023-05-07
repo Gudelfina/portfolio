ca8fa32d830a9a2149c7ea7f1657d8849ad7d78a
Gudelfina <gudelfina.mendez@gmail.com>
2023-05-06 23:55:33 -0600

    removed test temporarily

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const AboutPage = () => {
+  const current = <p>I'm currently a Software engineer</p>;
+  const
+  const tech_stack = [
+    "Python",
+    "Javascript ES6+",
+    "React.js",
+    "Django",
+    "HTML & CSS",
+  ];
   return (
     <div>
       <p>About page</p>

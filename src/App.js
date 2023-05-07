5449158d4db7989f1988f0e2581c6e5085b3af44
Gudelfina <gudelfina.mendez@gmail.com>
2023-05-06 23:18:20 -0600

    Added starting components and dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Intro from "./components/Intro.js";
+import Home from "./components/Home.js";
+import AboutPage from "./components/About.js";
+import Experience from "./components/Experience.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      {/* <AboutPage />
+      <Experience /> */}
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="container ">
+      <div>
+        <p>Hello, world. I'm Gudelfina.</p>
+        <p>
+          I'm a registered nurse turned software engineer <br />
+          based in Wyoming. I have a great interest in <br />
+          full-stack development, artificial intelligence, and everything in
+          between.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;

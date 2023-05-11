db12887edef89e16d97d72091067ee758ec2eb8d
Gudelfina <gudelfina.mendez@gmail.com>
2023-05-11 01:24:31 -0600

    Added project logos and small screen view

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 
 import "../styles/Projects.css";
+import gitlab from "../images/gitlab.png";
+import task from "../images/task.png";
+import playboxx from "../images/playboxx.png";
+import car from "../images/car.png";
 
 Modal.setAppElement("body");
 
@@ -13,18 +17,21 @@ const Projects = () => {
       techStack:
         "FastAPI, Javascript, Python, React.js, Redux, PostgresSQL, TailwindCSS",
       link: "https://gitlab.com/playboxx/play-boxx",
+      image: playboxx,
     },
     Project2: {
       title: "AutoTech",
       desc: "An automobile dealership that manages sales, services, and inventory. It allows users to create, view, and delete services based on the input into the system.",
       techStack: "React.js, Django, Docker, Javascript, Python",
       link: "https://gitlab.com/Gudelfina3/autotech",
+      image: car,
     },
     Project3: {
       title: "Task Manager",
       desc: "A managment application to create, update, and view projects and tasks for each project.",
       techStack: "Python, Django, HTML, CSS",
       link: "https://gitlab.com/Gudelfina3/project-management-app",
+      image: task,
     },
   };
 
@@ -44,12 +51,15 @@ const Projects = () => {
   return (
     <div className="projects" id="projects-nav">
       <h1 className="projects-header">/projects</h1>
-      <div className="project-container grid grid-cols-3 gap-10 ">
+      <div className="project-container lg:grid lg:grid-cols-3 lg:gap-10 md:grid md:grid-cols-3 md:gap-10 sm:flex sm:flex-wrap sm:content-center sm:gap-100">
         {Object.keys(allProjects).map((key, i) => (
           <ul key={i}>
             <li className="project-card flex-row justify-center align-center text-center h-full bg-white border border-gray-400 rounded-lg transform transition duration-700 ease-in-out hover:scale-110 hover:drop-shadow-2xl">
               <button className="card-btn" onClick={() => handleModalOpen(key)}>
-                <div className="card-title m-8">{allProjects[key].title}</div>
+                <div className="card-title m-8">
+                  {allProjects[key].title}
+                  <img src={allProjects[key].image} alt="project image" />
+                </div>
               </button>
             </li>
           </ul>
@@ -86,7 +96,11 @@ const Projects = () => {
                 {selectedProject["desc"]}
               </div>
               <div className="text-black">{selectedProject["techStack"]}</div>
-              <div className="text-black">{selectedProject["link"]}</div>
+              <div className="text-black">
+                <a href={selectedProject["link"]} target="blank">
+                  <img src={gitlab} alt="gitlab icon" width="20" height="20" />
+                </a>
+              </div>
               {console.log(selectedProject)}
             </div>
           )}

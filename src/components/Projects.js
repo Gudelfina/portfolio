e09d9829ce13de28bf7d79d8ecdffeed6e34b68e
Gudelfina <gudelfina.mendez@gmail.com>
2023-07-03 19:17:35 -0600

    Added portfolio project modal

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,31 +6,40 @@ import "../styles/Projects.css";
 import task from "../images/task.png";
 import playboxx from "../images/playboxx.png";
 import car from "../images/car.png";
+import project from "../images/project.png";
 
 Modal.setAppElement("body");
 
 const allProjects = {
   Project1: {
     title: "PlayBoxx",
-    desc: "A gaming site where players can play multiple games (one quiz game for now), user authentication, profile page, and leaderboard(s), etc.",
+    desc: "PlayBoxx is a personal, collaborative software engineer project that serves as a modern gaming website. Currently, it offers a captivating quiz game with ten questions, timed for added excitement. Users' scores are determined by their completion time and the number of correct answers. A leaderboard showcases the top ten scores, while users can view their own top ten scores on their profile page. PlayBoxx aims to provide an immersive gaming experience with engaging gameplay and comprehensive player information.",
     techStack:
       "FastAPI, Javascript, Python, React.js, Redux, PostgresSQL, TailwindCSS",
     link: "https://gitlab.com/playboxx/play-boxx",
+    websiteLink: "https://playboxx.gitlab.io/play-boxx/",
     image: playboxx,
   },
   Project2: {
     title: "AutoTech",
-    desc: "An automobile dealership that manages sales, services, and inventory. It allows users to create, view, and delete services based on the input into the system.",
+    desc: "Auto Tech is designed to work for a car dealership to manage their inventory, service appointments, and sales.",
     techStack: "React.js, Django, Docker, Javascript, Python",
     link: "https://gitlab.com/Gudelfina3/autotech",
     image: car,
   },
   Project3: {
     title: "Task Manager",
-    desc: "A managment application to create, update, and view projects and tasks for each project.",
+    desc: "This project management tool that helps individuals effectively manage their projects and associated tasks. This user-friendly application allows users to easily add, edit, and delete projects and tasks, enabling them to stay organized and track their progress efficiently.",
     techStack: "Python, Django, HTML, CSS",
     link: "https://gitlab.com/Gudelfina3/project-management-app",
     image: task,
+  },
+  Project4: {
+    title: "My Portfolio",
+    desc: "This website serves as a comprehensive showcase of my professional journey, highlighting my skills, experience, and projects.",
+    techStack: "Javascript, React, TailwindCSS, CSS",
+    link: "https://gitlab.com/Gudelfina3/portfolio",
+    image: project,
   },
 };
 
@@ -94,6 +103,18 @@ const Projects = () => {
               </div>
               <div className="modal-text-style" id="modal-techstack">
                 {selectedProject["techStack"]}
+              </div>
+              <div className="modal-text-style" id="modal-websiteLink">
+                {selectedProject && selectedProject.websiteLink && (
+                  <a href={selectedProject.websiteLink} target="black">
+                    <Icon
+                      icon="ph:link-bold"
+                      color="#b5838d"
+                      width="24"
+                      height="24"
+                    />
+                  </a>
+                )}
               </div>
               <div className="modal-link">
                 <a href={selectedProject["link"]} target="blank">

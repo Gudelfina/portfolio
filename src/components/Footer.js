180cfa6399943a86ce89fe7fd7cbb33ef997fdd9
Gudelfina <gudelfina.mendez@gmail.com>
2023-07-03 22:20:18 -0600

    removed fade in section from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from "react";
-import FadeInSection from "./FadeInSection";
 
 const Footer = () => {
   return (
     <div className="footer text-center pt-5 pb-20 sm:pb-20 md:pb-10 lg:pb-10">
-      <FadeInSection>
-        <div className="credits text-sm pb-0 text-greenNeon">
-          Copyright © Gudelfina Mendez
-        </div>
-        <div className="goodbye text-sm text-greenNeon">Have a great day!</div>
-      </FadeInSection>
+      <div className="credits text-sm pb-0 text-greenNeon">
+        Copyright © Gudelfina Mendez
+      </div>
+      <div className="goodbye text-sm text-greenNeon">Have a great day!</div>
     </div>
   );
 };
